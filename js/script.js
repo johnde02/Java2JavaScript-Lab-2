f85339289ddec5dbe80cf907f6de01fea9ec449c
Devon Lindsay <devon@MacBook-Pro.local>
2016-09-30 12:36:47 -0400

    made speak

--- js/script.js
+++ js/script.js
@@ -3,16 +3,34 @@
 function PetChat(){
   var display = document.getElementById("display");
   var numPets = null;
+  var voice = "Honeybadger doesn't care. "
+  function Pet (type, name, voice) {
+    this.type = type;
+    this.name = name;
+    this.voice = voice;
+  }
 
   var askHowMany = function(){
     return prompt("How many pets do you have?");
   };
 
+  function speak(type) {
+    if(type == "cat") {
+      voice = "meow!";
+    } else if (type == "dog") {
+      voice = "woof!";
+    } else if ( type == "bird") {
+      voice = "tweet!";
+    } else { voice = "Honeybadger don't care."}
+  }
+
   var askPetInfo = function(){
     for(var i=0; i<numPets; i++){
       var type = prompt("What is pet #" + (i+1) + "? (cat, dog, bird)");
       var name = prompt("What is pet #" + (i+1) + "'s name'?");
-      var textOut = type + " " + name + "<br />";
+      speak(type);
+      var textOut = type + " " + name + " " + voice + "<br />";
+      var myPet = new Pet(type, name, voice);
       display.innerHTML += textOut;
     }
   };
@@ -21,10 +39,11 @@ function PetChat(){
     while(numPets === null){
       numPets = askHowMany();
     }
-
-    askPetInfo();
+  askPetInfo();
   }
 };
+
+
 
 var chat = new PetChat();
 chat.init();
